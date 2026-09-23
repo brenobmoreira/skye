1c44cf3204c95c97d239c6230c427aef0aa063a7
Breno Barroso Moreira <brenobmoreira20@gmail.com>
2026-09-23 20:30:01 -0300

    make the web mode installable as an app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,5 +3,10 @@ import '@fontsource/jetbrains-mono/400.css';
 import '@xterm/xterm/css/xterm.css';
 import './styles.css';
 import { App } from './App';
+import { isWindow } from './bridge';
 
 createRoot(document.getElementById('root')!).render(<App />);
+
+if (!isWindow() && 'serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').catch(() => {});
+}
